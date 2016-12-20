9b89dd9a6754b8009614946fd01425577785fab0
TheArtist <agorglouk@gmail.com>
2016-12-20 04:47:56 +0200

    Mainloop now uses constant time algorithm

--- src/mainloop.c
+++ src/mainloop.c
@@ -1,23 +1,21 @@
 #include "mainloop.h"
+#include <tinycthread.h>
 #include <prof.h>
 
 void mainloop(struct mainloop_data* loop_data)
 {
-    timepoint_t next_update = millisecs();
-    const float skip_ticks = 1000 / loop_data->updates_per_second;
-
-    int loops;
-    float interpolation;
-
+    const float skip_ticks = 1000.0f / loop_data->updates_per_second;
     while (!loop_data->should_terminate) {
-        loops = 0;
-        while (millisecs() > next_update && loops < loop_data->max_frameskip) {
-            loop_data->update_callback(loop_data->userdata, skip_ticks);
-            next_update += skip_ticks;
-            ++loops;
+        timepoint_t t1 = millisecs();
+        loop_data->update_callback(loop_data->userdata, skip_ticks);
+        loop_data->render_callback(loop_data->userdata, 1.0f);
+        timepoint_t t2 = millisecs();
+        long long remaining = skip_ticks - (t2 - t1);
+        if (remaining > 0) {
+            struct timespec ts;
+            ts.tv_sec = 0;
+            ts.tv_nsec = 1000000 * remaining;
+            thrd_sleep(&ts, 0);
         }
-
-        interpolation = (millisecs() + skip_ticks - next_update) / skip_ticks;
-        loop_data->render_callback(loop_data->userdata, interpolation);
     }
 }
